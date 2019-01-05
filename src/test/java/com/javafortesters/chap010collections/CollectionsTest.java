aefbccb7577b8744b1d8cadfc27c819f8a30cd73
denderov <denderov@gmail.com>
2019-01-05 22:16:20 +0300

    End of list and set in chapter ten

--- src/test/java/com/javafortesters/chap010collections/CollectionsTest.java
+++ src/test/java/com/javafortesters/chap010collections/CollectionsTest.java
@@ -24,22 +24,65 @@ public class CollectionsTest {
 
     @Test
     public void userCollections() {
+
         Collection<User> usersB = new ArrayList<>();
         assertEquals(0,usersB.size());
+        assertTrue(usersB.isEmpty());
+
         addUsersB(usersB);
         assertEquals(2,usersB.size());
         assertFalse(usersB.isEmpty());
+
         Collection<User> usersA = new ArrayList<>();
         addUsersA(usersA);
         assertEquals(2,usersA.size());
         assertFalse(usersA.isEmpty());
+
         usersB.addAll(usersA);
         assertTrue(usersB.containsAll(usersA));
         assertEquals(4,usersB.size());
+
         usersA.removeAll(usersB);
         assertTrue(usersA.isEmpty());
+
         usersB.clear();
         assertTrue(usersB.isEmpty());
+
+    }
+
+    @Test
+    public void userList() {
+
+        List<User> users = new ArrayList<>();
+        User bob = new User("bob","b1");
+        User bill = new User("bill","b2");
+        assertEquals(0,users.size());
+        assertTrue(users.isEmpty());
+
+        users.add(bob);
+        assertEquals(1,users.size());
+        assertEquals(0,users.indexOf(bob));
+
+        users.add(0,bill);
+        assertEquals(2,users.size());
+        assertEquals(0,users.indexOf(bill));
+        assertEquals(1,users.indexOf(bob));
+
+        users.remove(0);
+        assertEquals(1,users.size());
+        assertEquals(0,users.indexOf(bob));
+
+    }
+
+    @Test
+    public void userSet() {
+        Set<User> users = new HashSet<>();
+        assertTrue(users.isEmpty());
+        User bob = new User("bob","b1");
+        users.add(bob);
+        assertEquals(1,users.size());
+        users.add(bob);
+        assertEquals(1,users.size());
     }
 
     private void addUsersB(Collection<User> users) {
@@ -51,4 +94,6 @@ public class CollectionsTest {
         users.add(new User("ann","a1"));
         users.add(new User("andy","a2"));
     }
+
+
 }
