828ed1f38c12e40310e0ea5d3bf82c4b53e6ea67
denderov <denderov@gmail.com>
2019-01-13 15:09:29 +0300

    End of chapter fourteen.

--- src/test/java/com/javafortesters/chap014junit/TestJUnit.java
+++ src/test/java/com/javafortesters/chap014junit/TestJUnit.java
@@ -1,10 +1,14 @@
 package com.javafortesters.chap014junit;
 
+
 import org.junit.Test;
 
 import java.util.Arrays;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.*;
+
 
 public class TestJUnit {
 
@@ -21,5 +25,29 @@ public class TestJUnit {
         assertNotSame(new Integer(1),new Integer(1));
         assertNull(null);
         assertSame('*', (char)42);
+    }
+
+    public void junitThatTest() {
+
+        assertThat(1,is(1));//assertEquals - check expected and actual are equal
+        assertThat(false,is(false)); //assertFalse - check actual is false
+        assertThat(true,is(true)); //assertTrue - check actual is true
+        int[] a = {1,2,3,4,5};
+        int[] b = {4,2,5,1,3};
+        Arrays.sort(b);
+        assertThat(a,equalTo(b)); //assertArrayEquals - check expected and actual arrays are equal
+        assertThat(1,is(not(null))); //assertNotNull - check actual is not null
+        assertThat(1,is(not(2))); //assertNotSame - check expected and actual are different
+        assertThat(null,is(nullValue())); //assertNull - check actual is null
+        assertThat(1,is(1)); //assertSame - check expected and actual are the same
+
+    }
+
+    public void hamcrestTest() {
+
+        assertThat("abc",containsString("b"));
+        assertThat("abc",startsWith("ab"));
+        assertThat("abc",endsWith("c"));
+
     }
 }
