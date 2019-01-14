e9412241472d42f0a40328622718526886f3db27
denderov <denderov@gmail.com>
2019-01-14 23:51:26 +0300

    Begin of chapter fifteen.

---
+++ src/test/java/com/javafortesters/chap015strings/StringsTest.java
@@ -0,0 +1,11 @@
+package com.javafortesters.chap015strings;
+
+import org.junit.Test;
+
+public class StringsTest {
+
+    @Test
+    public void escapeTest() {
+        System.out.println("Testing \\r \r;\\n \n; \\t \t;  \\b \b;");
+    }
+}
