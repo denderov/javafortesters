c5cd8e59e679a41a8b35f24dd67e66c7714160da
denderov <denderov@gmail.com>
2019-01-17 17:51:10 +0300

    Strings revisited.

--- src/test/java/com/javafortesters/chap015strings/StringsTest.java
+++ src/test/java/com/javafortesters/chap015strings/StringsTest.java
@@ -2,10 +2,23 @@ package com.javafortesters.chap015strings;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class StringsTest {
 
     @Test
     public void escapeTest() {
         System.out.println("Testing \\r \r;\\n \n; \\t \t;  \\b \b;");
+    }
+
+    @Test
+    public void constructTest() {
+        assertThat(new String("Test"), is("Test"));
+        char[] charArray = {'T','e','s','t'};
+        assertThat(new String(charArray), is("Test"));
+        String testStr = new String(charArray);
+        byte[] byteArray = testStr.getBytes();
+        System.out.println(byteArray);
     }
 }
