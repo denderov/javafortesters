07113a09280c0478098a720eaf5f5371786e348c
denderov <denderov@gmail.com>
2019-01-29 17:20:41 +0300

    End of chapter nineteen.

--- src/test/java/com/javafortesters/chap19files/testFiles.java
+++ src/test/java/com/javafortesters/chap19files/testFiles.java
@@ -3,8 +3,13 @@ package com.javafortesters.chap19files;
 import org.junit.Test;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.util.Objects;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class testFiles {
 
@@ -35,6 +40,92 @@ public class testFiles {
         outputFile = writeTestDataFile(".txt");
         System.out.println(outputFile.getName());
         outputFile.delete();
+
+    }
+
+    @Test
+    public void testRoots() {
+        File[] roots= File.listRoots();
+        for (File f:
+             roots) {
+            System.out.println(f.getAbsolutePath());
+        }
+    }
+
+    @Test
+    public void testMakeFile() throws IOException {
+
+        File tmpFile = new File(System.getProperty("java.io.tmpdir"), "File" + System.currentTimeMillis());
+        boolean newFile = tmpFile.createNewFile();
+        assertTrue(tmpFile.exists());
+        assertTrue(tmpFile.getName().matches("^File\\d+$"));
+        assertTrue(tmpFile.getPath().startsWith(System.getProperty("java.io.tmpdir")));
+        assertTrue(tmpFile.getParent().matches(System.getProperty("java.io.tmpdir")));
+        System.out.println(tmpFile.getName());
+        System.out.println(tmpFile.getPath());
+
+    }
+
+    @Test
+    public void testPaths() throws IOException {
+
+        File canonicalPath = new File("C:/1");
+        File path1 = new File("C:/1/2/3/4/../../..");
+        File path2 = new File("C:/1/2/../../1");
+        System.out.println(canonicalPath.getAbsolutePath());
+        System.out.println(path1.getAbsolutePath());
+        System.out.println(path2.getAbsolutePath());
+        assertEquals(canonicalPath.getAbsolutePath(),path1.getCanonicalPath());
+        assertEquals(canonicalPath.getAbsolutePath(),path2.getCanonicalPath());
+
+    }
+
+    @Test
+    public void testIsTempDirectoryIsDirectory() {
+
+        File tmpDir = new File(System.getProperty("java.io.tmpdir"));
+        assertTrue(tmpDir.isDirectory());
+
+    }
+
+    @Test
+    public void testFileWriter() throws IOException {
+
+        File file = File.createTempFile("test", null);
+
+        PrintWriter printWriter = new PrintWriter(file);
+        printWriter.println("line 1");
+        printWriter.close();
+
+        PrintWriter printWriterAppend = new PrintWriter(new BufferedWriter(new FileWriter(file,true)));
+        printWriterAppend.println("line 2");
+        printWriterAppend.close();
+
+    }
+
+    @Test
+    public void testListFiles() {
+
+        File tmpDir = new File(System.getProperty("java.io.tmpdir"));
+        File[] files = tmpDir.listFiles();
+        for (File f:
+                Objects.requireNonNull(files)) {
+            System.out.println((f.isDirectory()?"DIR:":"FIL:")+f.getName()+
+                    " read:"+f.canRead()+" write:"+f.canWrite()+" execute:"+f.canExecute()+
+                    " modified:"+f.lastModified());
+        }
+
+    }
+
+    @Test
+    public void testCopyAndMove() throws IOException {
+
+        File fileSource = writeTestDataFile();
+        File fileDestination = new File(fileSource.getCanonicalPath() + ".copy");
+        Files.copy(fileSource.toPath(), fileDestination.toPath());
+
+        File fileMoved = new File(fileSource.getCanonicalPath() + ".move");
+        Files.move(fileSource.toPath(), fileMoved.toPath());
 
     }
 
